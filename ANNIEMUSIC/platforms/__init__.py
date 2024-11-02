4b74b78d60128d3998d92d85a427238d9e4d1ccd
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-11-02 13:38:52 +0530

    Add files via upload

---
+++ ANNIEMUSIC/platforms/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2024 by Moonshining1@Github, < https://github.com/Moonshining1 >.
+#
+# This file is part of < https://github.com/Moonshining1/ANNIE-MUSIC > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Moonshining1/ANNIE-MUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
