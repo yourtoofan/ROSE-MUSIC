547dde70a6470bb7b1d3c5d65c082a7295fc6ae5
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-11-02 15:42:25 +0530

    Add files via upload

---
+++ ANNIEMUSIC/utils/database/__init__.py
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 by Moonshining1@Github, < https://github.com/Moonshining1 >.
+#
+# This file is part of < https://github.com/Moonshining1/ANNIE-MUSIC > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Moonshining1/ANNIE-MUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from .assistantdatabase import *
+from .memorydatabase import *
+from .mongodatabase import *
