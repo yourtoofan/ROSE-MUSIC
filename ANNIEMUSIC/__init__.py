f88e7acbafbcab19920e6fb5de4179a369879178
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-11-02 17:16:19 +0530

    Update __init__.py

--- ANNIEMUSIC/__init__.py
+++ ANNIEMUSIC/__init__.py
@@ -5,7 +5,7 @@ from ANNIEMUSIC.core.bot import ANNIEBot
 from ANNIEMUSIC.core.dir import dirr
 from ANNIEMUSIC.core.git import git
 from ANNIEMUSIC.core.userbot import Userbot
-from ANNIEMUSIC.core.youtube import ANNIEboy
+from ANNIEMUSIC.core.youtube import anniegirl
 from ANNIEMUSIC.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
